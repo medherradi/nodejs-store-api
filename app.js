bc4a3fdbd42e7ac26dac2befe5de1864f71d1aa9
medherradi <med.herradi@gmail.com>
2023-02-25 15:53:45 -0100

    setup mongoDB connection

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const connectDB = require('./db/connect')
 const notFound = require('./middleware/not-found')
 const errorHandler = require('./middleware/error-handler')
 const app = express()
 
+const mongoDBConnection = process.env.MONGODB_CONNECTION
 
+app.use(express.json())
 
 
 
@@ -12,3 +15,18 @@ const app = express()
 app.use('*', notFound)
 
 app.use(errorHandler)
+
+
+const port = process.env.PORT || 3000
+
+const startDB = async () => {
+  try {
+    await connectDB(mongoDBConnection)
+    console.log('connection to mongodb atlas ...')
+    app.listen(port, console.log(`server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startDB()
